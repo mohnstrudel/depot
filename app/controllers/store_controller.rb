563ad5aca90dc75f1b96b148890c47fa5f0ec537
Anton Kostin <schnibba@gmail.com>
2015-04-18 18:35:31 +0300

    Add user authentication and authorized-check

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
 	include CurrentCart
 	before_action :set_cart 
+	skip_before_action :authorize
 
 	def increment_counter
 		if session[:counter].nil?
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+password ""
+  end
+
+end
