fb0bf3828e094cd2477a53a12053703ad20889b4
Anton Kostin <schnibba@gmail.com>
2015-05-10 13:16:21 +0300

    Removed depot from socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 deploy_to  = "/home/deployer/apps/depot"
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
-socket_file= "#{deploy_to}/shared/unicorn.depot.sock"
+socket_file= "#{deploy_to}/shared/unicorn.sock"
 log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
